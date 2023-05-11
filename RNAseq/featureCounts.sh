cebda1f2343afc2304dc0493250683c65d12a7bb
liuyinzhe <54565139+liuyinzhe@users.noreply.github.com>
2023-05-11 17:35:26 +0800

    Create featureCounts.sh

---
+++ RNAseq/featureCounts.sh
@@ -0,0 +1,22 @@
+while read sampleA sampleB ; do 
+
+echo -e "#!/bin/bash 
+cd  ${PWD}
+PATH=/software/miniconda3/bin:$PATH; \\
+featureCounts \\
+  -Q 50 \\
+  --primary \\
+  --ignoreDup \\
+  --nonSplitOnly \\
+  -T 2 \\
+  -p \\
+  -g gene_id \\
+  -a /data/database/genome/ref.gtf \\
+  -o all.counts.${sampleA}_vs_${sampleB}.txt \\
+   ${sampleA}.sorted.bam \\
+   ${sampleB}.sorted.bam" > featureCounts_${sampleA}_vs_${sampleB}.sh
+
+
+done < pair_lst
+
+ls  featureCounts_*.sh | xargs -i echo sh {} > run.sh
