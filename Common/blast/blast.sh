a487207c7eb0ae5b1b553f424b71172eed53cf3d
liuyinzhe <54565139+liuyinzhe@users.noreply.github.com>
2023-08-21 10:29:47 +0800

    Update blast.sh

--- Common/blast/blast.sh
+++ Common/blast/blast.sh
@@ -2,3 +2,14 @@
 makeblastdb -in  all_database.fa -dbtype prot   -input_type fasta  -out db
 
 blastp -db db -query all.faa -outfmt 6 -evalue 1e-5 -num_threads 4  -out result.blast
+
+
+# Blast++与Diamond区别之一 qcovs
+# https://www.jianshu.com/p/9856fdce267b
+##Blast++
+#--outfmt  '6 qseqid sseqid pident length mismatch gapopen qlen qstart qend slen sstart send evalue bitscore qcovs qcovhsp'
+
+#Diamond
+#--outfmt  6 qseqid sseqid pident length mismatch gapopen qlen qstart qend slen sstart send evalue bitscore qcovhsp
+
+#注意细节，Diamond不需要单引号，少了一个 qcovs 的计算，源码也是没写
