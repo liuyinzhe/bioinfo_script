b0370e97a0361ad2f1f8107d5acf7a1a433818ce
liuyinzhe <54565139+liuyinzhe@users.noreply.github.com>
2023-10-09 14:38:08 +0800

    Update NT_NR.downloads.sh

--- Common/database/NT_NR.downloads.sh
+++ Common/database/NT_NR.downloads.sh
@@ -4,20 +4,22 @@
 #https://ftp.ncbi.nlm.nih.gov/blast/db/
 for i in {00..82}
 do
-    wget -c https://ftp.ncbi.nlm.nih.gov/blast/db/nr.${i}.tar.gz
-    wget -c https://ftp.ncbi.nlm.nih.gov/blast/db/nr.${i}.tar.gz.md5
-    md5sum -c nr.${i}.tar.gz.md5
-    tar -zxvf nr.${i}.tar.gz -C
-    echo "nr.${i} has done."
+    wget -c -o nr.${i}.wget.log https://ftp.ncbi.nlm.nih.gov/blast/db/nr.${i}.tar.gz
+    wget -c -o nr.${i}.md5.wget.log https://ftp.ncbi.nlm.nih.gov/blast/db/nr.${i}.tar.gz.md5
+    md5sum -c nr.${i}.tar.gz.md5 && \
+    tar -zxvf nr.${i}.tar.gz  && \
+    echo "nr.${i} has done." && \
+    rm -rf nr.${i}.tar.gz
 done
 
 # # NT
 # #https://ftp.ncbi.nlm.nih.gov/blast/db/
 # for i in {00..113}
 # do
-#     /bin/wget -c https://ftp.ncbi.nlm.nih.gov/blast/db/nt.${i}.tar.gz
-#     /bin/wget -c https://ftp.ncbi.nlm.nih.gov/blast/db/nt.${i}.tar.gz.md5
-#     md5sum -c nt.${i}.tar.gz.md5
-#     tar -zxvf nt.${i}.tar.gz -C
-#     echo "nt.${i} has done."
+#     /bin/wget -c -o nr.${i}.wget.log https://ftp.ncbi.nlm.nih.gov/blast/db/nt.${i}.tar.gz
+#     /bin/wget -c -o nr.${i}.md5.wget.log https://ftp.ncbi.nlm.nih.gov/blast/db/nt.${i}.tar.gz.md5
+#     md5sum -c nt.${i}.tar.gz.md5 && \
+#     tar -zxvf nt.${i}.tar.gz  && \
+#     echo "nt.${i} has done." && \
+#     rm -rf nt.${i}.tar.gz
 # done
