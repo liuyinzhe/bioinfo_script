8db64c35d0df67fcc6767943aa8e19151dd441fb
liuyinzhe <54565139+liuyinzhe@users.noreply.github.com>
2023-10-19 10:05:19 +0800

    Update seqkit_split2.sh

--- Common/seqkit/seqkit_split2.sh
+++ Common/seqkit/seqkit_split2.sh
@@ -1,6 +1,7 @@
+# For FASTQ files (paired-end)
 seqkit split2  \
   --by-part  3 \
   --force  \
   -1 sample.clean.1.fq.gz \
   -2 sample.clean.2.fq.gz \
-  -o suffix   # suffix.part_003.fq.gz
+  -O split_result
