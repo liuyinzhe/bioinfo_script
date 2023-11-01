6b6d814e43e138f4ab1af4b6f9fcf2b395b11e9f
liuyinzhe <54565139+liuyinzhe@users.noreply.github.com>
2023-11-01 13:44:37 +0800

    Create ClassifyCNV_run.sh

---
+++ Common/ClassifyCNV/ClassifyCNV_run.sh
@@ -0,0 +1,12 @@
+python3 ClassifyCNV.py \
+   --infile Examples/ACMG_examples.hg19.bed \
+   --GenomeBuild hg19 \
+   --precise \
+   --outdir ./xxx && \
+cut -f 1-7,43-  ./xxx/Scoresheet.txt > ClassifyCNV_ann.tsv
+
+# ≤ −0.99:   benign variant
+# −0.90 .. −0.98:   likely benign variant
+# −0.89 .. 0.89:   variant of uncertain significance
+# 0.90 .. 0.98:   likely pathogenic variant
+# ≥ 0.99:   pathogenic variant
