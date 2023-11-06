1a5240e64d3f59a54d39bdd9449d668a6951316a
liuyinzhe <54565139+liuyinzhe@users.noreply.github.com>
2023-11-06 16:51:12 +0800

    Update metaphlan_database.sh

--- microbe/taxonomy/metaphlan/metaphlan_database.sh
+++ microbe/taxonomy/metaphlan/metaphlan_database.sh
@@ -3,3 +3,9 @@ http://cmprod1.cibio.unitn.it/biobakery4/metaphlan_databases/mpa_vOct22_CHOCOPhl
 http://cmprod1.cibio.unitn.it/biobakery4/metaphlan_databases/mpa_vOct22_CHOCOPhlAnSGB_202212.tar \
 http://cmprod1.cibio.unitn.it/biobakery4/metaphlan_databases/mpa_vOct22_CHOCOPhlAnSGB_202212_marker_info.txt.bz2 \
 http://cmprod1.cibio.unitn.it/biobakery4/metaphlan_databases/mpa_vOct22_CHOCOPhlAnSGB_202212_species.txt.bz2
+
+
+
+bowtie2-build --threads 20 \
+  -f mpa_vOct22_CHOCOPhlAnSGB_202212.fna \
+  mpa_vOct22_CHOCOPhlAnSGB_202212
