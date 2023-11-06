950aaf18b0f5cb8913a6d2b8214fd039d86d9a7e
liuyinzhe <54565139+liuyinzhe@users.noreply.github.com>
2023-11-06 16:38:08 +0800

    Create metaphlan_database.sh

---
+++ microbe/taxonomy/metaphlan/metaphlan_database.sh
@@ -0,0 +1,5 @@
+aria2c -c -Z \
+http://cmprod1.cibio.unitn.it/biobakery4/metaphlan_databases/mpa_vOct22_CHOCOPhlAnSGB_202212.md5 \
+http://cmprod1.cibio.unitn.it/biobakery4/metaphlan_databases/mpa_vOct22_CHOCOPhlAnSGB_202212.tar \
+http://cmprod1.cibio.unitn.it/biobakery4/metaphlan_databases/mpa_vOct22_CHOCOPhlAnSGB_202212_marker_info.txt.bz2 \
+http://cmprod1.cibio.unitn.it/biobakery4/metaphlan_databases/mpa_vOct22_CHOCOPhlAnSGB_202212_species.txt.bz2
