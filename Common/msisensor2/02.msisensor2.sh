a11c1ee2356a599a48ac36448fcaf790123885c3
liuyinzhe <54565139+liuyinzhe@users.noreply.github.com>
2023-11-14 13:49:07 +0800

    Update 02.msisensor2.sh

--- Common/msisensor2/02.msisensor2.sh
+++ Common/msisensor2/02.msisensor2.sh
@@ -1,5 +1,8 @@
 /usr/bin/time -v /data/software/msisensor2-master/msisensor2 msi \
+  -c 20 \
   -d hg19.microsatelittes.site  \
   -t tumor.sorted.markdup.BQSR.bam \
   -n normal.sorted.markdup.BQSR.bam \
   -o tumor  >log 2>msisensor2.sum
+
+#-c   <int>      coverage threshold for msi analysis, WXS: 20; WGS: 15, default=20
