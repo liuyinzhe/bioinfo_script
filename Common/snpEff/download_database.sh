90f3e23d8264ff8797d823390675e97dc209f18a
liuyinzhe <54565139+liuyinzhe@users.noreply.github.com>
2023-11-14 20:02:40 +0800

    Create download_database.sh

---
+++ Common/snpEff/download_database.sh
@@ -0,0 +1,9 @@
+
+### databases
+
+ java -Xmx8g -jar /data/software/snpEff/snpEff.jar databases  >snpEff.databases.list.txt
+
+
+### download 
+java -Xmx8g -jar /data/software/snpEff/snpEff.jar download  -v  hg38
+java -Xmx8g -jar /data/software/snpEff/snpEff.jar download  -v  hg19 
