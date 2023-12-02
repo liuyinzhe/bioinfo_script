bd15c1360fd016d03c7b43dd3be5da4a9ce93d17
liuyinzhe <54565139+liuyinzhe@users.noreply.github.com>
2023-12-02 10:09:23 +0800

    Update run_unicycler.sh

--- Common/unicycler/run_unicycler.sh
+++ Common/unicycler/run_unicycler.sh
@@ -1,13 +1,14 @@
 
-# ngs
+# ngs PE
 unicycler \
+  --keep 1 \
   --no_pilon \
   --no_correct \
   --min_kmer_frac 0.6 \
   --max_kmer_frac 0.99 \
   --kmer_count 3 \
   --min_fasta_length 500 \
-  -t 10 \
-  -1 sample.1.fastq.gz \
-  -2 sample.2.fastq.gz \
-  -o spades_out/sample 
+  --threads 8 \
+  --short1 sample.1.fastq.gz \
+  --short2 sample.2.fastq.gz \
+  --out spades_out/sample 
