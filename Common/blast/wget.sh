8a9829dde650c252f63065fe853e0d9aa7559a18
liuyinzhe <54565139+liuyinzhe@users.noreply.github.com>
2024-03-19 16:18:08 +0800

    Create wget.sh

---
+++ Common/blast/wget.sh
@@ -0,0 +1,4 @@
+wget https://ftp.ncbi.nlm.nih.gov/pub/taxonomy/taxdmp.zip
+wget https://ftp.ncbi.nlm.nih.gov/pub/taxonomy/accession2taxid/dead_nucl.accession2taxid.gz
+
+wget https://ftp.ncbi.nlm.nih.gov/pub/taxonomy/accession2taxid/nucl_gb.accession2taxid.gz
