296949347e0976a2cdac6fba0aaa0625020e5ec6
liuyinzhe <54565139+liuyinzhe@users.noreply.github.com>
2024-03-21 15:55:53 +0800

    Create metaphlan_databases.sh

---
+++ Databases/metaphlan/metaphlan_databases.sh
@@ -0,0 +1,12 @@
+aria2c -c \
+http://cmprod1.cibio.unitn.it/biobakery4/metaphlan_databases/mpa_vJun23_CHOCOPhlAnSGB_202307.tar \
+http://cmprod1.cibio.unitn.it/biobakery4/metaphlan_databases/mpa_vJun23_CHOCOPhlAnSGB_202307.md5 \
+http://cmprod1.cibio.unitn.it/biobakery4/metaphlan_databases/mpa_vJun23_CHOCOPhlAnSGB_202307_marker_info.txt.bz2 \
+http://cmprod1.cibio.unitn.it/biobakery4/metaphlan_databases/mpa_vJun23_CHOCOPhlAnSGB_202307_species.txt.bz2 \
+http://cmprod1.cibio.unitn.it/biobakery4/metaphlan_databases/bowtie2_indexes/mpa_vJun23_CHOCOPhlAnSGB_202307_bt2.tar \
+http://cmprod1.cibio.unitn.it/biobakery4/metaphlan_databases/bowtie2_indexes/mpa_vJun23_CHOCOPhlAnSGB_202307_bt2.md5 \
+>log2 2>err2
+
+# 以http://cmprod1.cibio.unitn.it/biobakery4/metaphlan_databases/mpa_latest 记录版本为准，日期新的不一定能用
+# tar -xvf mpa_vJun23_CHOCOPhlAnSGB_202307_bt2.tar
+# tar -xvf mpa_vJun23_CHOCOPhlAnSGB_202307.tar
