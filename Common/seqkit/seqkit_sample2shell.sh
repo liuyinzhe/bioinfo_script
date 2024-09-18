2398d4fb7641c4c75ff41f74e132f3352f6c9bd8
liuyinzhe <54565139+liuyinzhe@users.noreply.github.com>
2024-09-18 09:46:15 +0800

    Create seqkit_sample2shell.sh

---
+++ Common/seqkit/seqkit_sample2shell.sh
@@ -0,0 +1,14 @@
+rm -rf batch_seqkit_sample.sh
+mkdir -p sample
+while read sample ;do
+
+ echo -e "seqkit sample  \
+ --rand-seed 123 \
+ --proportion 0.9 \
+ ${sample}.1.fq.gz  |seqkit head -n 5000  |gzip > sample/${sample}.1.fq.gz 
+seqkit sample  \
+ --rand-seed 123 \
+ --proportion 0.9 \
+ ${sample}.2.fq.gz  |seqkit head -n 5000  |gzip > sample/${sample}.2.fq.gz  " >> batch_seqkit_sample.sh
+
+done <sample.lst
