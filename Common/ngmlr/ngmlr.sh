21981cc5e8bb9fe1a210a1d8e5d6b30a668623b2
liuyinzhe <54565139+liuyinzhe@users.noreply.github.com>
2024-10-11 13:21:56 +0800

    Update ngmlr.sh

--- Common/ngmlr/ngmlr.sh
+++ Common/ngmlr/ngmlr.sh
@@ -5,3 +5,8 @@ reference=ref.fa
 out_bam=sample.sorted.bam
 ngmlr -t ${thread} --skip-write -x ont -r ${reference} -q ${query} | \
 samtools sort -@ ${thread} -T tmp_$task_id -o ${out_bam} -  >log 2>err
+
+
+
+#    -x <pacbio, ont>,  --presets <pacbio, ont>
+#        Parameter presets for different sequencing technologies [pacbio]
