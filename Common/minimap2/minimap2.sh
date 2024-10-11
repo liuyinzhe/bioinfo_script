a6a9f81a9e0be1cc58722af2307115ee4ba808c6
liuyinzhe <54565139+liuyinzhe@users.noreply.github.com>
2024-10-11 13:22:45 +0800

    Update minimap2.sh

--- Common/minimap2/minimap2.sh
+++ Common/minimap2/minimap2.sh
@@ -5,3 +5,13 @@ reference=ref.fa
 out_bam=sample.sorted.bam
 minimap2 -t ${thread} -L --MD -Y -a -x map-pb ${reference} ${query} | \
     samtools sort -@ ${thread} -T tmp_$task_id -o ${out_bam} - >log 2>err
+
+
+#  Preset:
+#    -x STR       preset (always applied before other options; see minimap2.1 for details) []
+#                 - map-pb/map-ont - PacBio CLR/Nanopore vs reference mapping
+#                 - map-hifi - PacBio HiFi reads vs reference mapping
+#                 - ava-pb/ava-ont - PacBio/Nanopore read overlap
+#                 - asm5/asm10/asm20 - asm-to-ref mapping, for ~0.1/1/5% sequence divergence
+#                 - splice/splice:hq - long-read/Pacbio-CCS spliced alignment
+#                 - sr - genomic short-read mapping
