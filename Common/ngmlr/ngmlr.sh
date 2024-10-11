522beecdafc3eaa93c1b3d2ac24384e66d55b898
liuyinzhe <54565139+liuyinzhe@users.noreply.github.com>
2024-10-11 13:20:10 +0800

    Create ngmlr.sh

---
+++ Common/ngmlr/ngmlr.sh
@@ -0,0 +1,7 @@
+thread = 12
+task_id=project10
+query=read.fq.gz
+reference=ref.fa
+out_bam=sample.sorted.bam
+ngmlr -t ${thread} --skip-write -x ont -r ${reference} -q ${query} | \
+samtools sort -@ ${thread} -T tmp_$task_id -o ${out_bam} -  >log 2>err
