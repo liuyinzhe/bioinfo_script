8ae2fa1df540d7b58a10240f09e0529884eedd9b
liuyinzhe <54565139+liuyinzhe@users.noreply.github.com>
2024-10-11 13:17:30 +0800

    Create minimap2.sh

---
+++ Common/minimap2/minimap2.sh
@@ -0,0 +1,7 @@
+thread=12
+task_id=project10
+query=read.fq.gz
+reference=ref.fa
+out_bam=sample.sorted.bam
+minimap2 -t ${thread} -L --MD -Y -a -x pbava ${reference} ${query} | \
+    samtools sort -@ ${thread} -T tmp_$task_id -o ${out_bam} - >log 2>err
