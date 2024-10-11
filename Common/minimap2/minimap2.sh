54971618cd74ae8f2593d482501a4483243efe2f
liuyinzhe <54565139+liuyinzhe@users.noreply.github.com>
2024-10-11 13:19:19 +0800

    Update minimap2.sh

--- Common/minimap2/minimap2.sh
+++ Common/minimap2/minimap2.sh
@@ -3,5 +3,5 @@ task_id=project10
 query=read.fq.gz
 reference=ref.fa
 out_bam=sample.sorted.bam
-minimap2 -t ${thread} -L --MD -Y -a -x pbava ${reference} ${query} | \
+minimap2 -t ${thread} -L --MD -Y -a -x map-pb ${reference} ${query} | \
     samtools sort -@ ${thread} -T tmp_$task_id -o ${out_bam} - >log 2>err
