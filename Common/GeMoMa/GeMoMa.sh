210f30c3cb5c7175a9c007e43bf83c00d9f71c20
liuyinzhe <54565139+liuyinzhe@users.noreply.github.com>
2025-02-20 10:55:56 +0800

    Create GeMoMa.sh

---
+++ Common/GeMoMa/GeMoMa.sh
@@ -0,0 +1,8 @@
+/data/software/Genefamily_pipeline-master/GeMoMaOneSpPipeline.sh \
+  tblastn \
+  /data/assembly_genome/SRR.fasta \
+  /data/assembly_genome/ref/genomic.gff \
+  /data/assembly_genome/ref/genomic.fna \
+  8 \
+  GeMoMa_out \
+  1e-10 
