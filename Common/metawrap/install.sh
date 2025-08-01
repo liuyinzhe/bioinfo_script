094ac123cff76335f92efd967cd89cd1a826d49f
liuyinzhe <54565139+liuyinzhe@users.noreply.github.com>
2025-08-01 17:16:52 +0800

    Update install.sh

--- Common/metawrap/install.sh
+++ Common/metawrap/install.sh
@@ -5,3 +5,10 @@ conda config --add channels bioconda
 conda config --add channels ursky
 
 mamba create -n metawrap python=2.7  metawrap-mg
+
+#modify
+#/envs/metawrap/bin/config-metawrap
+
+# bug : metawrap  --show-config
+# ImportError: No module named _sysconfigdata_x86_64_conda_linux_gnu
+cp -a /data/xx/miniconda3/pkgs/python-2.7/lib/python2.7/_sysconfigdata_x86_64_conda_linux_gnu.py* /data/yy/miniconda3/envs/metawrap/lib/python2.7/
