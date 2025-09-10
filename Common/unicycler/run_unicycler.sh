9d737db977b00117c49ccdf62a7ef10623991a82
liuyinzhe <54565139+liuyinzhe@users.noreply.github.com>
2025-09-10 09:50:14 +0800

    Update run_unicycler.sh

--- Common/unicycler/run_unicycler.sh
+++ Common/unicycler/run_unicycler.sh
@@ -1,14 +1,11 @@
 
 # ngs PE
+sample=sample
+export PATH=/data/envs/unicycler/bin:$PATH;
+cd /data/project/assembly/unicycler
 unicycler \
-  --keep 1 \
-  --no_pilon \
-  --no_correct \
-  --min_kmer_frac 0.6 \
-  --max_kmer_frac 0.99 \
-  --kmer_count 3 \
-  --min_fasta_length 500 \
-  --threads 8 \
-  --short1 sample.1.fastq.gz \
-  --short2 sample.2.fastq.gz \
-  --out spades_out/sample 
+ --min_fasta_length 500 \
+ -1 /data/project/assembly/unicycler/01.qc/${sample}/${sample}.1.fq.gz \
+ -2 /data/project/assembly/unicycler01.qc/${sample}/${sample}.2.fq.gz \
+ -t 10 \
+ -o /data/project/assembly/unicycler/02.assembly/${sample} 
