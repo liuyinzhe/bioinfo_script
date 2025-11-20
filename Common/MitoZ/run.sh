0ee38ec9c2fa41ae92089cee07a60f41b42240fd
liuyinzhe <54565139+liuyinzhe@users.noreply.github.com>
2025-11-20 16:09:43 +0800

    Create run.sh

---
+++ Common/MitoZ/run.sh
@@ -0,0 +1,16 @@
+mitoz annotate \
+  --outprefix mitoz \
+  --species_name 'latin name' \
+  --fastafiles  mitochondrion.fasta \
+  --clade Chordata
+
+# clade
+# Chordata	脊索动物门
+# Arthropoda	节肢动物门
+# Echinodermata	棘皮动物门
+# Annelida-segmented-worms	环节动物门 分节蠕虫
+# Bryozoa	苔藓动物门
+# Mollusca	软体动物门
+# Nematoda	线形动物门
+# Nemertea-ribbon-worms 纽形动物门 带状蠕虫
+# Porifera-sponges	多孔动物门 海绵
