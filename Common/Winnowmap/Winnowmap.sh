c9171ce0c8702ab23a2bd5f65b653f57aefa957e
liuyinzhe <54565139+liuyinzhe@users.noreply.github.com>
2025-12-17 13:27:03 +0800

    Create Winnowmap.sh

---
+++ Common/Winnowmap/Winnowmap.sh
@@ -0,0 +1,16 @@
+# Mapping ONT or PacBio-hifi WGS reads
+
+meryl count k=15 output merylDB ref.fa
+meryl print greater-than distinct=0.9998 merylDB > repetitive_k15.txt
+
+winnowmap -W repetitive_k15.txt -ax map-ont ref.fa ont.fq.gz > output.sam
+#winnowmap -W repetitive_k15.txt -ax map-pb ref.fa hifi.fq.gz > output.sam
+
+
+
+# Mapping genome assemblies
+
+meryl count k=19 output merylDB asm1.fa
+meryl print greater-than distinct=0.9998 merylDB > repetitive_k19.txt
+
+winnowmap -W repetitive_k19.txt -ax asm20 asm1.fa asm2.fa > output.sam
