c7d070df51fb22fb1342c097e877a4635640475a
liuyinzhe <54565139+liuyinzhe@users.noreply.github.com>
2026-05-04 11:44:58 +0800

    Create install.sh

---
+++ Common/RSeQC/install.sh
@@ -0,0 +1,6 @@
+pip install rseqc
+#conda install rseqc
+
+#conda search ucsc-gtftogenepred  
+#wget -c http://hgdownload.cse.ucsc.edu/admin/exe/linux.x86_64/genePredToBed
+#wget -c http://hgdownload.cse.ucsc.edu/admin/exe/linux.x86_64/gtfToGenePred
