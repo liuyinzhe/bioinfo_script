4669f30a9e362d823bf116749e1e8767ad5fbd11
liuyinzhe <54565139+liuyinzhe@users.noreply.github.com>
2026-05-09 15:41:14 +0800

    Update bwa2_sorted_rmdup.sh

--- Common/sambamba/bwa2_sorted_rmdup.sh
+++ Common/sambamba/bwa2_sorted_rmdup.sh
@@ -8,5 +8,10 @@ sample=AAAAA
   ${sample}.clean.1.fq \
   ${sample}.clean.2.fq \
   | sambamba view -q -t 4 -f bam -S /dev/stdin -l 0 -o /dev/stdout \
-  | sambamba sort  -q --tmpdir=${PWD}/${sample}/tmp -t 4 /dev/stdin -o  ${sample}/${sample}.sorted.bam  && \
-   sambamba markdup -q --tmpdir=${PWD}/tmp  -t 4 ${sample}/${sample}.sorted.bam ${sample}/${sample}.sorted.rmdup.bam && rm -rf ${sample}/${sample}.sorted.bam*
+  | sambamba sort  -q -m 8G --tmpdir=${PWD}/${sample}/tmp -t 4 /dev/stdin -o  ${sample}/${sample}.sorted.bam  && \
+   sambamba markdup -q   --hash-table-size 524288 --overflow-list-size 600000 \
+  --tmpdir=${PWD}/tmp  -t 4 ${sample}/${sample}.sorted.bam ${sample}/${sample}.sorted.rmdup.bam && rm -rf ${sample}/${sample}.sorted.bam*
+
+# sambamba  markdup   in mode `w+' (Too many open files)
+# https://github.com/biod/sambamba/issues/177
+# try --overflow-list-size 600000; There is no upper threshold, and another suggestion is to increase --hash-table-size as well
