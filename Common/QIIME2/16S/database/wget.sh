9961daced2daf96c33dc82608f9512ff2fcbd66d
liuyinzhe <54565139+liuyinzhe@users.noreply.github.com>
2026-07-08 15:57:49 +0800

    Create wget.sh

---
+++ Common/QIIME2/16S/database/wget.sh
@@ -0,0 +1,15 @@
+#https://wanjinhu.github.io/2023/10/27/pipeline-qiime2-analysis/
+# https://www.arb-silva.de/archive/release_138.1/Exports/
+wget https://www.arb-silva.de/fileadmin/silva_databases/release_138.1/Exports/SILVA_138.1_SSURef_NR99_tax_silva_trunc.fasta.gz
+wget https://www.arb-silva.de/fileadmin/silva_databases/release_138.1/Exports/SILVA_138.1_SSURef_NR99_tax_silva_trunc.fasta.gz.md5
+
+wget https://www.arb-silva.de/fileadmin/silva_databases/release_138.1/Exports/taxonomy/tax_slv_ssu_138.1.txt.gz 
+wget https://www.arb-silva.de/fileadmin/silva_databases/release_138.1/Exports/taxonomy/tax_slv_ssu_138.1.txt.gz.md5
+
+
+wget https://www.arb-silva.de/fileadmin/silva_databases/release_138.1/Exports/taxonomy/taxmap_slv_ssu_ref_nr_138.1.txt.gz
+wget https://www.arb-silva.de/fileadmin/silva_databases/release_138.1/Exports/taxonomy/taxmap_slv_ssu_ref_nr_138.1.txt.gz.md5
+
+
+wget https://www.arb-silva.de/fileadmin/silva_databases/release_138.1/Exports/taxonomy/tax_slv_ssu_138.1.tre.gz
+wget https://www.arb-silva.de/fileadmin/silva_databases/release_138.1/Exports/taxonomy/tax_slv_ssu_138.1.tre.gz.md5
