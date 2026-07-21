840a880572f76cb584d6fd8cbe90841602816339
liuyinzhe <54565139+liuyinzhe@users.noreply.github.com>
2026-07-21 10:28:37 +0800

    Create qiime2.2026.4_install.sh

---
+++ Common/QIIME2/16S/env/qiime2.2026.4_install.sh
@@ -0,0 +1,8 @@
+
+conda env create \
+  --name qiime2 \
+  --file https://raw.githubusercontent.com/qiime2/distributions/refs/heads/dev/2026.4/qiime2/released/rachis-qiime2-linux-64-conda.yml
+
+
+#     $ conda activate qiime2
+#     $ conda deactivate
